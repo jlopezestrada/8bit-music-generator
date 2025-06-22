371c8be217f58aec5a50d6e1d7c5366fb6321462
jlopezestrada <jose.a.l.e@hotmail.com>
2025-06-22 15:39:37 +0200

    fix: add system dependencies to Dockerfile

--- backend/app/player.py
+++ backend/app/player.py
@@ -2,3 +2,8 @@ import sounddevice as sd
 import numpy as np
 
 SAMPLE_RATE = 44100
+
+def play_note(frequency: float, duration: float, volume: float = 0.5):
+    if frequency <= 0:
+        sd.sleep(int(duration * 1000))
+        return
