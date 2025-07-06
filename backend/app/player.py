e1a684e0859f82e4dd82e83ebd40abaf4b8ecba6
Jose Angel Lopez <jose.a.l.e@hotmail.com>
2025-07-06 12:34:32 +0200

    feat: working generated file

--- backend/app/player.py
+++ backend/app/player.py
@@ -1,13 +1,46 @@
 import sounddevice as sd
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+
+from app.notes import note_to_frequency
+from app.generator import generate_melody
+from scipy.io.wavfile import write
 
 SAMPLE_RATE = 44100
 
-def generate_wave(frequency: float, duration: float, volume: float = 0.5) -> np.ndarray:
+def generate_wave(frequency: float, duration: float, volume: float = 0.05) -> np.ndarray:
     if frequency == 0:
         return np.zeros(int(SAMPLE_RATE * duration), dtype=np.float32)
     
     t = np.linspace(0, duration, int(SAMPLE_RATE * duration), endpoint=False)
     wave = volume * np.sign(np.sin(2 * np.pi * frequency * t))
     return wave.astype(np.float32)
+
+def play_note(frequency: float, duration: float, volume: float = 0.05):
+    wave = generate_wave(frequency, duration, volume)
+    sd.play(wave, samplerate=SAMPLE_RATE)
+    sd.wait()
+
+def play_melody(melody: list, volume: float = 0.05):
+    full_wave = _generate_full_wave(melody, volume)
+    sd.play(full_wave, samplerate=SAMPLE_RATE)
+    sd.wait()
+
+def save_melody(melody: list, filename="melody.wav", volume: float = 0.05):
+    full_wave = np.concatenate([
+        generate_wave(note_to_frequency(note), duration, volume)
+        for note, duration in melody
+    ])
+
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    wav_path = os.path.join(base_dir, "wav")
+    os.makedirs(wav_path, exist_ok=True)
+
+    full_path = os.path.join(wav_path, filename)
+    print(full_path)
+    write(full_path, SAMPLE_RATE, full_wave)
+    
+def _generate_full_wave(melody: list, volume: float = 0.05) -> np.ndarray:
+    return np.concatenate([
+        generate_wave(note_to_frequency(note), duration, volume) for note, duration in melody
+    ])
--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,14 @@
-# TODO
 from app.generator import generate_melody
+from app.player import save_melody
 
 def main():
     print("8-Bit Music Generator")
+    melody = generate_melody(length=25, start_octave=3, end_octave=5)
 
-    melody = generate_melody(length=150, start_octave=3, end_octave=5)
-    print("Generated melody:")
-    for note_name, duration in melody:
-        print(f" - {note_name}, duration: {duration}s")
+    for note, duration in melody:
+        print(f" - {note}, duration: {duration}s")
+
+    save_melody(melody, volume=0.05)
 
 if __name__ == "__main__":
     main()
